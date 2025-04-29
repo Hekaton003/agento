997f224a83a48e722f8af0c29c9ff8663d725497
Jovan <jovandavaliev@gmail.com>
2025-04-29 13:53:01 +0200

    New Major Changes

--- Lab-vezbi/Lab3/zad3.py
+++ Lab-vezbi/Lab3/zad3.py
@@ -1,7 +1,42 @@
 import gymnasium as gym
 import numpy as np
+import torch.nn as nn
 from PIL import Image
-from deep_q_learning_torch import DuelingDQN
+from deep_q_learning_torch import DQN
+
+
+def eval_agent(env, agent, iterations, render=False):
+    total_reward = 0
+    for i in range(iterations):
+        state, _ = env.reset()
+        preprocessed_state = preprocess_state(state)
+        terminated = False
+        episode_reward = 0
+        while not terminated:
+            if render:
+                env.render()
+            action = agent.get_action(preprocessed_state, 0.0)
+            next_state, reward, terminated,truncated,info = env.step(action)
+            new_preprocessed_state = preprocess_state(new_state)
+            episode_reward += reward
+            preprocessed_state = new_preprocessed_state
+
+        total_reward += episode_reward
+
+    avg_reward = total_reward / iterations
+    print(f"\n=== Evaluation Result ===")
+    print(f"Average reward over {iterations} iterations: {avg_reward:.2f}")
+
+
+def build_model(input_channels, num_actions):
+    return nn.Sequential(
+        nn.Conv2d(input_channels, 32, kernel_size=8, stride=4),
+        nn.ReLU(),
+        nn.Conv2d(32, 64, kernel_size=3, stride=1),
+        nn.ReLU(),
+        nn.Flatten(),
+        nn.LazyLinear(num_actions)
+    )
 
 
 def preprocess_state(state):
@@ -17,42 +52,44 @@ def preprocess_reward(reward):
 
 
 if __name__ == '__main__':
-    from gymnasium.envs import registry
-
-    # Print all available environment IDs
-    for env_id in registry:
-        print(env_id)
     num_iterations = int(input('Select number of iterations: '))
-    envs = gym.envs.registry
-    for env in envs:
-        print(env)
-    env = gym.make('ALE/MsPacman-v5')
+    env = gym.make('ALE/MsPacman-v5', render_mode="rgb_array")
     state, _ = env.reset()
-    print(env.action_space.n)
-    print(env.observation_space.shape)
-    """
-    state_space_shape = (1, env.observation_space.shape[0], env.observation_space.shape[1])
+    state_space_shape = env.observation_space.shape
     num_actions = env.action_space.n
 
-    agent = DuelingDQN(state_space_shape=state_space_shape, num_actions=num_actions)
+    agent = DQN(state_space_shape=state_space_shape, num_actions=num_actions,model=build_model(1,num_actions),
+                target_model=build_model(1,num_actions))
 
     num_episodes = 100
     epsilon = 0.5
 
     for episode in range(num_episodes):
+        if episode >= num_episodes * 0.6:
+            epsilon = epsilon * 0.06
         preprocessed_state = preprocess_state(state)
-        action = agent.get_action(preprocessed_state, epsilon)
+        terminated = False
+        while not terminated:
+            action = agent.get_action(preprocessed_state, epsilon)
+            new_state, reward, terminated, _, _ = env.step(action)
+            new_preprocessed_state = preprocess_state(new_state)
+            agent.update_memory(preprocessed_state, action, reward, new_preprocessed_state, terminated)
 
-        new_state, reward, terminated, _, _ = env.step(action)
-        new_preprocessed_state = preprocess_state(new_state)
-
-        agent.update_memory(preprocessed_state, action, reward, new_preprocessed_state, terminated)
-
-        state = new_state
+            preprocessed_state=new_preprocessed_state
 
         if (episode + 1) % 5 == 0:
             agent.train()
 
         if (episode + 1) % 20 == 0:
             agent.update_target_model()
-    """
+
+    eval_agent(env,agent,num_iterations)
+
+"""
+=== Evaluation Result ===
+Average reward over 50 iterations: 70.00
+"""
+"""
+=== Evaluation Result ===
+Average reward over 100 iterations: 210.00
+"""
